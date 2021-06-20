8f02160eefe5596da4269a8f9c28ca0afeda211e
dungbik <q1w2e30630@gmail.com>
2021-06-20 13:55:22 +0900

    파일을 정적으로 사용 가능하게 수정

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/static", express.static(__dirname + "/uploads"));
+
 app.use(router);
 
 app.use(notFound);
